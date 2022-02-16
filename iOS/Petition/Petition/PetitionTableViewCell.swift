ad135bb820c7498948bc10dda14fd083ac7061de
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-02-16 20:14:55 -0300

    parsing JSON using the Codable protocol

---
+++ iOS/Petition/Petition/PetitionTableViewCell.swift
@@ -0,0 +1,66 @@
+//
+//  PetitionTableViewCell.swift
+//  Petition
+//
+//  Created by Grazielli Berti on 16/02/22.
+//
+
+import UIKit
+
+class PetitionTableViewCell: UITableViewCell {
+    //MARK: - Variable
+    static let reuseIdentifier =  "petitionTableViewCell"
+
+    //MARK: - View
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let detailLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12, weight: .light)
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    fileprivate func setupView() {
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(detailLabel)
+    }
+    
+    fileprivate func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            
+            detailLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4),
+            detailLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            detailLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            detailLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+
+        ])
+    }
+    
+    func configureCell(data: Petition) {
+        titleLabel.text = data.title
+        titleLabel.text = data.body
+    }
+}
---
+++ iOS/Petition/Petition/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  Petition
+//
+//  Created by Grazielli Berti on 16/02/22.
+//
+
+import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+    //MARK: Variable
+    var petitions = [Petition]()
+    let cellHeight: CGFloat = 50
+    
+    //MARK: Views
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(PetitionTableViewCell.self, forCellReuseIdentifier: PetitionTableViewCell.reuseIdentifier)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = UITableView.automaticDimension
+        
+        setupView()
+        configureConstrains()
+        loadData()
+    }
+    
+    fileprivate func loadData() {
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parseOurJSONIntoArray(json: data)
+                print(data)
+            }
+        }
+    }
+    
+    fileprivate func parseOurJSONIntoArray(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            print(jsonPetitions)
+            tableView.reloadData()
+        }
+    }
+    
+    fileprivate func setupView() {
+        view.addSubview(tableView)
+    }
+    
+    fileprivate func configureConstrains() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+//MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PetitionTableViewCell.reuseIdentifier, for: indexPath) as? PetitionTableViewCell else { return UITableViewCell() }
+        let petition = petitions[indexPath.row]
+        cell.titleLabel.text = petition.title
+        cell.detailLabel.text = petition.body
+        return cell
+    }
+}
+
+//MARK: - UITableViewDelegate
+extension UIViewController: UITableViewDelegate {
+}
