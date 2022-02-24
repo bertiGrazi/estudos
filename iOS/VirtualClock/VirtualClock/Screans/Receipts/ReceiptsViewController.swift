5384c5fe8920366990a721b970cc37e09e15c7ec
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-02-24 08:53:58 -0300

    create view picture

--- iOS/VirtualClock/VirtualClock/Screans/Receipts/ReceiptsViewController.swift
+++ iOS/VirtualClock/VirtualClock/Screans/Receipts/ReceiptsViewController.swift
@@ -8,9 +8,34 @@
 import UIKit
 
 class ReceiptsViewController: UIViewController {
-
+    //MARK: - Variable
+    
+    //MARK: - View
+    let viewPicture: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemPink
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .link
+        view.backgroundColor = .white
+        setupView()
+        setupControllers()
+    }
+    
+    fileprivate func setupView() {
+        view.addSubview(viewPicture)
+    }
+    
+    fileprivate func setupControllers() {
+        NSLayoutConstraint.activate([
+            viewPicture.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            viewPicture.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            viewPicture.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            viewPicture.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width),
+            viewPicture.heightAnchor.constraint(equalToConstant: 200),
+        ])
     }
 }
