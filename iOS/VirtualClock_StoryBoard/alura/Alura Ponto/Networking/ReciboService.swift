2390dbce132401143e3e26b3f292983af21a31f8
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-03-01 19:38:04 -0300

    start networking app

---
+++ iOS/VirtualClock_StoryBoard/alura/Alura Ponto/Networking/ReciboService.swift
@@ -0,0 +1,16 @@
+//
+//  ReciboService.swift
+//  Alura Ponto
+//
+//  Created by Grazielli Berti on 01/03/22.
+//
+
+import Foundation
+
+//Responsavel para criar as requisições que será salva
+class ReciboService {
+    func post(_recibo: Recibo) {
+        let baseURL = "http://localhost:8080/"
+        let path = "recibos"
+    }
+}
