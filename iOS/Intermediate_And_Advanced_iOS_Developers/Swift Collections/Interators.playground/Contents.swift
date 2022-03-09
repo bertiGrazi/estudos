fb6ada4d7c5b8342f7040f49325e57c7841787a7
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-03-09 11:56:53 -0300

    learning about makeIterator method and Interator Protocol

---
+++ iOS/Intermediate_And_Advanced_iOS_Developers/Swift Collections/Interators.playground/Contents.swift
@@ -0,0 +1,67 @@
+import UIKit
+
+let names = ["Alex", "John", "Mary"]
+
+//The old Way
+for name in names {
+    print("Old Way: " + name)
+}
+
+print("\n")
+
+var nameIterator = names.makeIterator()
+
+while let name = nameIterator.next() {
+    print("Iterator Way: " + name)
+}
+
+/*The IteratorProtocol*/
+    //protocol IteratorProtocol {
+    //    associatedtype Element
+    //
+    //    mutating func next() -> Element?
+    //
+    //}
+
+print("\n")
+
+struct Countdown: Sequence {
+    let start: Int
+    
+    func makeIterator() -> some IteratorProtocol {
+        return CountdownIterator(self)
+    }
+}
+
+struct CountdownIterator: IteratorProtocol {
+    let countdown: Countdown
+    var currentValue = 0
+    
+    init(_ countdown: Countdown) {
+        self.countdown = countdown
+        self.currentValue = countdown.start
+    }
+    
+    mutating func next() -> Int? {
+        if currentValue > 0 {
+            let value = currentValue
+            currentValue -= 1
+            return value
+        } else {
+            return nil
+        }
+    }
+}
+
+let countdown = Countdown(start: 10)
+
+print("The old Way")
+for count in countdown {
+    print(count)
+}
+
+print("\nThe makeIterator mathod")
+var count = countdown.makeIterator()
+while let num = count.next() {
+    print(num)
+}
