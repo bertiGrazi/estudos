854481e4ea63c7f262b467a7dffc832ebd79b27d
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-03-13 11:24:49 -0300

    Create method adicionaArray and getQuantidadDeContas

--- Java/java.lang/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/GuardadosDeContas.java
+++ Java/java.lang/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/GuardadosDeContas.java
@@ -2,8 +2,34 @@ package br.com.bytebank.banco.modelo;
 
 public class GuardadosDeContas {
 	private Conta[] referencias;
+	private int posicaoLivre = 0; //Ja me fala quantos elementos tem
 	
 	public GuardadosDeContas() {
 		this.referencias = new Conta[10];
 	}
+	
+	public void adiciona(Conta ref) {
+		this.referencias[this.posicaoLivre] = ref;
+		this.posicaoLivre++;
+	}
+
+	public int getQuantidadeDeContas() {
+		return this.posicaoLivre;
+	}
+
+	public Conta getReferencia(int posicao) {
+		return this.referencias[posicao];
+	}
 }
+
+
+
+
+
+/*
+ * Para adicionar um Array e verificar a sua posicao
+ *	Passo 1) Criar uma funcao adiciona que ira adicionar a nova referencia no array
+ *	Passo 2) Criar uma variavel int com a posicao livre desse index
+ *	Passo 3) Adicionar essa variavel no index e fazer a incrementacao, pois ela inicia 
+ *com 0, e quando ele for ocupada devera ser adicionado. */
+ 
--- Java/java.lang/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteGuardadosDeContas.java
+++ Java/java.lang/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteGuardadosDeContas.java
@@ -1,6 +1,7 @@
 package br.com.bytebank.banco.test;
 
 import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.GuardadosDeContas;
 
 public class TesteGuardadosDeContas {
@@ -9,10 +10,20 @@ public class TesteGuardadosDeContas {
 		
 		GuardadosDeContas guardador = new GuardadosDeContas();
 		
-		Conta cc = new Conta(22, 11);
+		Conta cc = new ContaCorrente(22, 11);
+		Conta cc2 = new ContaCorrente(33, 22);
 		
-		//guardador.adiciona(cc);
-
+		guardador.adiciona(cc);
+		guardador.adiciona(cc2);
+		
+		int tamanho = guardador.getQuantidadeDeContas();
+		System.out.println("Quantidades de Contas Correntes Adicionas: " + tamanho);
+		
+		//Acessar os dados que temos guardados nos nossos Arrays
+		
+		//Quero pegar as referencia que tenho nos meus Array. A minha variavek
+		// tem que ser do tipo conta, porque eh o que eu quero acessar. 
+		Conta ref = guardador.getReferencia(0); //de quem eu quero
+		System.out.println("Numero da Conta: " + ref.getNumero()); //imprimindo o valor de quem eu quero
 	}
-
 }
