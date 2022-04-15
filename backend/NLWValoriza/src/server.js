d7fa5ca2be83ae85ff760f4d45568160f7480615
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-04-15 19:36:51 -0300

    feat: create router /test at port 8000

--- backend/NLWValoriza/src/server.js
+++ backend/NLWValoriza/src/server.js
@@ -5,7 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = (0, express_1.default)();
+/**
+ * GET => Buscar uma informação. Qualquer busca.
+ * POST => Inserir uma informação dentro da nossa API. Criação.
+ * PUT => Alterar uma informação. Manipulação dentro de uma dado já existente.
+ * DELETE => Deletar uma informacào
+ * PATCH => Alterar uma informação específica. Somente modificar foto, ou senha...
+ */
+//Primeiro Parâmetro: Rota 
+app.get("/test", function (req, res) {
+    return res.send("Olá NLW");
+});
 //Porta que vamos executar o projeto: http: //localhost: 3000
 app.listen(3000, function () {
-    return console.log("Server is running");
+    return console.log("Server is running NLW");
 });
--- backend/NLWValoriza/src/server.ts
+++ backend/NLWValoriza/src/server.ts
@@ -2,7 +2,20 @@ import express from "express"
 
 const app = express();
 
+/**
+ * GET => Buscar uma informação. Qualquer busca. 
+ * POST => Inserir uma informação dentro da nossa API. Criação. 
+ * PUT => Alterar uma informação. Manipulação dentro de uma dado já existente.
+ * DELETE => Deletar uma informacào
+ * PATCH => Alterar uma informação específica. Somente modificar foto, ou senha... 
+ */
+
+//Primeiro Parâmetro: Rota 
+app.get("/test", (req, res) => {
+  return res.send("Olá NLW Oi")
+})
+
 //Porta que vamos executar o projeto: http: //localhost: 3000
-app.listen(3000, () =>  { 
-  return console.log("Server is running NLW") 
+app.listen(8000, () =>  { 
+  return console.log("Server is running NLW Oi") 
 });
