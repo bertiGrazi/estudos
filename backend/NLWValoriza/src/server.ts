c624fa7ef37afb85c2d5cf3b023d40816264be5b
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-04-16 21:32:10 -0300

    feat: create a database sqlite

--- backend/NLWValoriza/src/server.ts
+++ backend/NLWValoriza/src/server.ts
@@ -1,27 +1,9 @@
+import "reflect-metadata"
 import express from "express"
+import "./database"
 
 const app = express();
 
-/**
- * GET => Buscar uma informação. Qualquer busca. 
- * POST => Inserir uma informação dentro da nossa API. Criação. 
- * PUT => Alterar uma informação. Manipulação dentro de uma dado já existente.
- * DELETE => Deletar uma informacào
- * PATCH => Alterar uma informação específica. Somente modificar foto, ou senha... 
- */
-
-//Primeiro Parâmetro: Rota 
-app.get("/test", (req, res) => {
-  return res.send("Olá NLW. ")
-})
-
-app.post("/test-post", (req, res) => 
-{
-  return res.send("Olá, NWL método POST")
-})
-
-
-//Porta que vamos executar o projeto: http: //localhost: 3000
 app.listen(8000, () =>  { 
   return console.log("Server is running NLW Oi") 
 });
