f35251dc6ef7bd71f00023eee474b5d2d0e74b66
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-05-07 15:37:41 -0300

    sucess login with google account

--- frontend/OAuth2 - React Native/oauth2app/src/screens/SignIn/index.tsx
+++ frontend/OAuth2 - React Native/oauth2app/src/screens/SignIn/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
+import * as AuthSession from 'expo-auth-session';
 import { useNavigation } from '@react-navigation/native';
 
 import { Button } from '../../components/Button';
@@ -11,6 +12,16 @@ export function SignIn() {
   const navigation = useNavigation();
 
   async function handleSignIn() {
+    const CLIENT_ID = '20585366123-caq31kam51kd2stea8s23j2u1u4v1spt.apps.googleusercontent.com';
+    const REDIRECT_URI = 'https://auth.expo.io/@testegraziberti/oauth2app';
+    const RESPONSE_TYPE = 'token';
+    const SCOPE = encodeURI('profile email');
+
+
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+    const responde = await AuthSession.startAsync({ authUrl })
+    console.log(responde)
+
     navigation.navigate('Profile');
   }
 
