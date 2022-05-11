3453427e6bdf507897dd703d4efc574341b60566
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-05-11 12:03:22 -0300

    start func addGreadent()

---
+++ frontend/iOS/Netflix/Nextflix/Nextflix/Views/HeroHeaderUIView.swift
@@ -0,0 +1,40 @@
+//
+//  HeroHeaderUIView.swift
+//  Nextflix
+//
+//  Created by Grazielli Berti on 11/05/22.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+    
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+        addGreadent()
+    }
+    
+    private func addGreadent() {
+        let grandientLayer = CAGradientLayer()
+        grandientLayer.colors
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
