b490f33cbf7babdb98e4f22a44cf10092a7a22a1
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-05-11 10:59:02 -0300

    feat: create TabBarController

---
+++ frontend/iOS/Netflix/Nextflix/Nextflix/Controllers/UpcomingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UpcomingViewController.swift
+//  Nextflix
+//
+//  Created by Grazielli Berti on 11/05/22.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
