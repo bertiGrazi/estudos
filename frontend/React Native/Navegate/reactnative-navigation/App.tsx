c4a449a76eb2fdddf5b3e5e954a106ee653c8640
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-05-14 20:20:49 -0300

    feat: routes navigation

--- frontend/React Native/Navegate/reactnative-navigation/App.tsx
+++ frontend/React Native/Navegate/reactnative-navigation/App.tsx
@@ -1,14 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-import { ScreenA } from './src/screens/ScreenA';
-import { ScreenB } from './src/screens/ScreenB';
-
-
+import Routes from "./src/routes"
 
 export default function App() {
   return (
-   <ScreenB />
+    <Routes />
   );
 }
--- frontend/React Native/Navegate/reactnative-navigation/src/screens/ScreenA/index.tsx
+++ frontend/React Native/Navegate/reactnative-navigation/src/screens/ScreenA/index.tsx
@@ -1,10 +1,20 @@
-import { ReactNode } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React  from 'react';
+import { View, Button } from 'react-native';
+import { useNavigation } from "@react-navigation/native"
 
 export function ScreenA() {
+  const navigation = useNavigation();
+
+  function openScreen() {
+    navigation.navigate('screenB');
+  }
+
   return (
-    <View style={{flex: 1, backgroundColor: 'red'}}>
-    
+    <View style={{flex: 1, backgroundColor: 'red', justifyContent: 'center'}}>
+      <Button
+        title='Ir para tela B'
+        onPress={openScreen}
+      />
     </View>
   );
 }
