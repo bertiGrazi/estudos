4b7bc0a3ec049564f166e923d621d12ceae79351
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-05-15 12:33:46 -0300

    setup header view

--- frontend/iOS/Netflix/Nextflix/Nextflix/Views/HeroHeaderUIView.swift
+++ frontend/iOS/Netflix/Nextflix/Nextflix/Views/HeroHeaderUIView.swift
@@ -9,23 +9,68 @@ import UIKit
 
 class HeroHeaderUIView: UIView {
     
+    let playButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let downloadButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Download", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     private let heroImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
         imageView.clipsToBounds = true
-        imageView.image = UIImage(named: "heroImage")
+        imageView.image = UIImage(named: "strangerThings_Image")
         return imageView
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(heroImageView)
+        setupView()
+        setupConstrains()
         addGreadent()
+    }
+    
+    private func setupView() {
+        addSubview(heroImageView)
+        heroImageView.addSubview(playButton)
+        heroImageView.addSubview(downloadButton)
+    }
+    
+    private func setupConstrains() {
+        NSLayoutConstraint.activate([
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 90),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            playButton.widthAnchor.constraint(equalToConstant: 100),
+            
+            downloadButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -90),
+            downloadButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            downloadButton.widthAnchor.constraint(equalToConstant: 100),
+        ])
     }
     
     private func addGreadent() {
         let grandientLayer = CAGradientLayer()
-        grandientLayer.colors
+        grandientLayer.colors = [
+            UIColor.clear.cgColor,
+//            UIColor.black.cgColor
+        ]
+        
+        grandientLayer.frame = bounds
+        layer.addSublayer(grandientLayer)
     }
     
     
