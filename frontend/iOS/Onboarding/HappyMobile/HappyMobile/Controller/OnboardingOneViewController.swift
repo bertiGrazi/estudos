a7e958d16ee17e55f4d331fae8b30c9a91861bf7
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-11-08 17:03:29 -0300

    feat: create a onboarding with UIPageViewController

---
+++ frontend/iOS/Onboarding/HappyMobile/HappyMobile/Controller/OnboardingOneViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingOneViewController.swift
+//  HappyMobile
+//
+//  Created by Grazi  Berti on 08/11/22.
+//
+
+import UIKit
+
+class OnboardingOneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .purple
+    }
+}
