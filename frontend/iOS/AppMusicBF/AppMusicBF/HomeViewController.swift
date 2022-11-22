46ecd766bc8ada64476b368b14681066eb1ef30f
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-11-22 12:08:14 -0300

    storyboard delete

---
+++ frontend/iOS/AppMusicBF/AppMusicBF/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AppMusicBF
+//
+//  Created by Grazi  Berti on 22/11/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
