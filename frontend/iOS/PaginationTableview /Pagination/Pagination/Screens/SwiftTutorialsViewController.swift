aa8ce3df0b0f12c02a2505222c9b3692126c578d
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-11-25 08:19:21 -0300

    start SwiftTutorialsViewController

--- frontend/iOS/PaginationTableview /Pagination/Pagination/Screens/SwiftTutorialsViewController.swift
+++ frontend/iOS/PaginationTableview /Pagination/Pagination/Screens/SwiftTutorialsViewController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class SwiftTutorialsViewController: UIViewController {
 
+    var recordsArray: [Int] = Array()
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .white
+//
+//        tableView.dataSource = self
+//        tableView.delegate = self
     }
 }
+//
+//extension SwiftTutorialsViewController: UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//
+//}
