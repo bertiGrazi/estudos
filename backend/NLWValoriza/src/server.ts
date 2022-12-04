8d63c956b0a2e2e89ea7c12ee2cf6614edabd2d0
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-12-04 14:05:38 -0300

    clean filders

--- backend/NLWValoriza/src/server.ts
+++
@@ -1,9 +0,0 @@
-import "reflect-metadata"
-import express from "express"
-import "./database"
-
-const app = express();
-
-app.listen(8000, () =>  { 
-  return console.log("Server is running NLW Oi") 
-});
--- frontend/JavaScript/DOM/script.js
+++
@@ -1,7 +0,0 @@
-const h1Selecionado = document.querySelector('h1');
-
-function callbackh1() {
-    console.log(h1Selecionado.innerHTML)
-}
-h1Selecionado.addEventListener('click', callbackh1);
-
