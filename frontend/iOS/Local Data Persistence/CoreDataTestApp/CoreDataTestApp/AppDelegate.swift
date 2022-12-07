f4f4a1bd307190391936ee497fd37aa9da59f6d5
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-12-07 10:19:40 -0300

    feat: save data with core data

--- frontend/iOS/Local Data Persistence/CoreDataTestApp/CoreDataTestApp/AppDelegate.swift
+++ frontend/iOS/Local Data Persistence/CoreDataTestApp/CoreDataTestApp/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  ToDoList
+//  CoreDataTestApp
 //
-//  Created by Grazi  Berti on 30/11/22.
+//  Created by Grazi  Berti on 07/12/22.
 //
 
 import UIKit
@@ -14,11 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        // Override point for customization after application launch.
         return true
     }
-
 
     // MARK: UISceneSession Lifecycle
 
@@ -33,10 +31,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-        self.saveContext()
-    }
 
     // MARK: - Core Data stack
 
@@ -47,7 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "ToDoList")
+        let container = NSPersistentContainer(name: "CoreDataTestApp")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
--- frontend/iOS/Local Data Persistence/ToDoList/ToDoList/Controller/ViewController.swift
+++ frontend/iOS/Local Data Persistence/ToDoList/ToDoList/Controller/ViewController.swift
@@ -6,13 +6,14 @@
 //
 
 import UIKit
+import CoreData
 
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var itensArray = [Item]()
     let defaults = UserDefaults.standard
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     private let tableView: UITableView = {
         let tableView = UITableView()
@@ -30,30 +31,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.dataSource = self
         
         tableView.frame = view.bounds
-        
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        newItem.done = true
-        itensArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggs"
-        itensArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Argentina"
-        itensArray.append(newItem3)
-        
-        let newItem4 = Item()
-        newItem4.title = "Brasil"
-        itensArray.append(newItem4)
-        
-//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-//            itensArray = items
-//        }
-        
+
         setupNavigation()
-        loadItems()
+        //loadItems()
     }
     
     fileprivate func setupNavigation() {
@@ -74,8 +54,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         let action = UIAlertAction(title: "Add Item", style: .default) { action in
             
-            let newItem = Item()
+            
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
+            newItem.done = false
             self.itensArray.append(newItem)
 
             self.saveItems()
@@ -93,11 +75,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     //MARK: - Model Manupulation Methods
     func saveItems() {
-        let encoder = PropertyListEncoder()
-        
         do {
-            let data = try encoder.encode(itensArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
+            
         } catch {
             print("Error encoding item array, \(error)")
         }
@@ -105,16 +85,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         self.tableView.reloadData()
     }
     
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                itensArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("\(error)")
-            }
-        }
-    }
+//    func loadItems() {
+//        if let data = try? Data(contentsOf: dataFilePath!) {
+//            let decoder = PropertyListDecoder()
+//            do {
+//                itensArray = try decoder.decode([Item].self, from: data)
+//            } catch {
+//                print("\(error)")
+//            }
+//        }
+//    }
     
     //MARK: - TableView DataSource Methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
