0b14a4d55c6a20515467cfcb200d4a42559c606c
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-01-02 13:46:10 -0300

    feat: create structure app

---
+++ frontend/iOS/Rick & Morty App/RickAndMortyApp/RickAndMortyApp/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Grazi  Berti on 02/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
