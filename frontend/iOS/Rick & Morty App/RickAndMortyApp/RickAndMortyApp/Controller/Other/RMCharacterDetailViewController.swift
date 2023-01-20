9f7180310f51bd20dca0fe2a19343ca36d51c67e
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-01-20 19:27:07 -0300

    call character detail with api

--- frontend/iOS/Rick & Morty App/RickAndMortyApp/RickAndMortyApp/Controller/Other/RMCharacterDetailViewController.swift
+++ frontend/iOS/Rick & Morty App/RickAndMortyApp/RickAndMortyApp/Controller/Other/RMCharacterDetailViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 final class RMCharacterDetailViewController: UIViewController {
     private let viewModel: RMCharacterDetailViewViewModel
 
+    private let detailView = RMCharacterDetailView()
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -26,5 +27,29 @@ final class RMCharacterDetailViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = viewModel.title
+        view.addSubview(detailView)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .action,
+            target: self,
+            action: #selector(didTapShare)
+        )
+        addConstrains()
+        viewModel.fetchCharacterData()
+    }
+    
+    
+    
+    @objc
+    func didTapShare() {
+        print(#function)
+    }
+    
+    fileprivate func addConstrains() {
+        NSLayoutConstraint.activate([
+            detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            detailView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
--- frontend/iOS/Rick & Morty App/RickAndMortyApp/RickAndMortyApp/View/RMCharacterDetailView.swift
+++ frontend/iOS/Rick & Morty App/RickAndMortyApp/RickAndMortyApp/View/RMCharacterDetailView.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import UIKit
+
+/// View for single character info
+final class RMCharacterDetailView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
